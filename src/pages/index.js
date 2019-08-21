592e09864632a7d8298749244dd99517fedea6ec
ETBox <13318972468@126.com>
2019-08-21 15:23:31 +0800

    feat: add Layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,40 @@
-import { version, Button } from 'antd';
+import { Layout, Menu, Icon } from 'antd';
 
-import styles from './index.css';
+// import styles from './index.css';
+
+const { Header, Content, Footer, Sider } = Layout;
 
 export default function() {
   return (
-    <div className={styles.normal}>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>
-          To get started, edit <code>src/pages/index.js</code> and save to reload.
-        </li>
-        <div>Current antd version: {version}</div>
-        <div style={{ marginTop: '16px' }}>
-          <Button type="primary">Example button</Button>
-        </div>
-      </ul>
-    </div>
+    <Layout style={{ height: '100%' }}>
+      <Sider breakpoint="lg" collapsedWidth="0">
+        <div className="logo" />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+          <Menu.Item key="1">
+            <Icon type="user" />
+            <span className="nav-text">nav 1</span>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Icon type="video-camera" />
+            <span className="nav-text">nav 2</span>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <Icon type="upload" />
+            <span className="nav-text">nav 3</span>
+          </Menu.Item>
+          <Menu.Item key="4">
+            <Icon type="user" />
+            <span className="nav-text">nav 4</span>
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout>
+        <Header style={{ background: '#fff', padding: 0 }}>Welcome to AntD</Header>
+        <Content style={{ margin: '24px 16px 0' }}>
+          <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>content</div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      </Layout>
+    </Layout>
   );
 }
