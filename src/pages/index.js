35b38d453d6de0b463ce0185ca148c3ae1cd7d2e
ETBox <13318972468@126.com>
2019-08-21 10:33:44 +0800

    feat: check antd can run

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,19 @@
-import styles from './index.css';
+import { version, Button } from 'antd';
 
+import styles from './index.css';
 
 export default function() {
   return (
     <div className={styles.normal}>
       <div className={styles.welcome} />
       <ul className={styles.list}>
-        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
         <li>
-          <a href="https://umijs.org/guide/getting-started.html">
-            Getting Started
-          </a>
+          To get started, edit <code>src/pages/index.js</code> and save to reload.
         </li>
+        <div>Current antd version: {version}</div>
+        <div style={{ marginTop: '16px' }}>
+          <Button type="primary">Example button</Button>
+        </div>
       </ul>
     </div>
   );
