bb07c550d6786f492072126d2c433b7e9282816c
ETBox <13318972468@126.com>
2019-08-22 14:27:36 +0800

    feat: add Select time

--- src/components/TableList.jsx
+++ src/components/TableList.jsx
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types'
+import { Select } from 'antd';
 
 import styles from './TableList.css';
+
+const { Option } = Select;
 
 export default class TableList extends Component {
   // static propTypes = {
   //   prop: PropTypes
   // }
 
+  constructor(props) {
+    super(props);
+
+    this.state = { value: 'hour' };
+  }
+
+  onChange = value => {
+    this.setState({ value });
+  };
+
   render() {
-    return <div className={styles.container}>233</div>;
+    return (
+      <div className={styles.container}>
+        时间：
+        <Select defaultValue="hour" onChange={this.onChange} style={{ width: 120 }}>
+          <Option value="hour">最近一小时</Option>
+          <Option value="day">最近一天</Option>
+          <Option value="week">最近一周</Option>
+          <Option value="other">其它时间</Option>
+        </Select>
+      </div>
+    );
   }
 }
