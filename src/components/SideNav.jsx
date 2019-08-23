1c63aa87090504ec0fb317e829a350d94838cc5b
ETBox <13318972468@126.com>
2019-08-23 15:03:47 +0800

    feat: add search nav

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { Menu } from 'antd';
+import { Menu, Select } from 'antd';
 // import PropTypes from 'prop-types'
 
 import logo from '../assets/logo-01.png';
 import styles from './Main.css';
+
+const { Option } = Select;
 
 export default class SideNav extends Component {
   // static propTypes = {
@@ -17,18 +19,29 @@ export default class SideNav extends Component {
           <img src={logo} alt="logo" />
           <h1>领翌科技</h1>
         </div>
+        <Select
+          defaultValue="hour"
+          // onChange={this.onChange}
+          showSearch
+          style={{ width: 120, marginBottom: 5, marginTop: 10 }}
+        >
+          <Option value="hour">分组1</Option>
+          <Option value="day">分组2</Option>
+          <Option value="week">分组3</Option>
+          <Option value="other">分组4</Option>
+        </Select>
         <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
           <Menu.Item key="1">
-            <span className="nav-text">nav 1</span>
+            <span className="nav-text">塔1</span>
           </Menu.Item>
           <Menu.Item key="2">
-            <span className="nav-text">nav 2</span>
+            <span className="nav-text">塔2</span>
           </Menu.Item>
           <Menu.Item key="3">
-            <span className="nav-text">nav 3</span>
+            <span className="nav-text">塔3</span>
           </Menu.Item>
           <Menu.Item key="4">
-            <span className="nav-text">nav 4</span>
+            <span className="nav-text">塔4</span>
           </Menu.Item>
         </Menu>
       </nav>
