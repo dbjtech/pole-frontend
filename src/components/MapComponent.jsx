b08d03669d16dae16f42244a4a42a312d775be16
ETBox <13318972468@126.com>
2019-08-28 17:44:27 +0800

    feat: add favicon.png

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types'
 import { Map, Marker } from 'react-amap';
 
 import { amap } from '../config';
 import styles from './Main.css';
 
 export default class MapComponent extends Component {
-  // static propTypes = {
-  //   prop: PropTypes
-  // }
-
   render() {
     return (
       <div className={styles.amap}>
@@ -19,11 +14,11 @@ export default class MapComponent extends Component {
           plugins={amap.plugins}
           center={amap.mapCenter}
         >
-          <div className={styles.promptContainer}>
+          {/* <div className={styles.promptContainer}>
             <span className={styles.normalDevice}>正常设备数：</span>
             <br />
             <span className={styles.abnormalDevice}>异常设备数：</span>
-          </div>
+          </div> */}
           <Marker position={amap.mapCenter} />
         </Map>
       </div>
